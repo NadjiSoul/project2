f78e63ca45cc68523a4c8e5a0839dae3c44e1d6f
Soulaimana Nadji <nadji.soulaimana@gmail.com>
2019-07-30 15:22:12 +0200

    Switch : Mobile -> Desktop

---
+++ js/java.js
@@ -0,0 +1,13 @@
+
+/* FONCTION D'AFFICHAGE DE TEXTE SOUS MOBILE */
+
+function divart(id){
+
+	var div = document.getElementById(id);
+
+	if(div.style.display == 'none'){
+		div.style.display = 'block';
+	} else {
+		div.style.display = 'none';
+	} 
+} 
